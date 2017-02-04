d399240097f5e98ca538eabd2aa78d349f9daf7d
banjocat <jackmuratore@gmail.com>
2017-02-04 02:49:34 -0500

    Add unit test framework

--- test.c
+++ test.c
@@ -1,5 +1,25 @@
+#include <stdio.h>
+#include "minunit.h"
 
+int tests_run = 0;
+
+static char *test_minunit() {
+        mu_assert("minunit failed", 1 == 1);
+        return 0;
+}
+
+static char *all_tests() {
+        mu_run_test(test_minunit);
+        return 0;
+}
 
 int main() {
-        return 0;
+        char *result = all_tests();
+        if (result != 0) {
+                printf("%s\n", result);
+        }
+        else {
+                printf("All tests passed!\n");
+        }
+        return result != 0;
 }
